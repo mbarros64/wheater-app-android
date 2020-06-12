a8b93b7dcf6de2ce44b3ffd04596a118cf37e610
matheus <barros.matheusprestes@gmail.com>
2020-06-12 18:09:56 -0400

    Add realm dependencies and realm object
    Signed-off-by: matheus <barros.matheusprestes@gmail.com>

---
+++ app/src/main/java/com/mbarros64/wheater_app_android/data/model/Forecast.kt
@@ -0,0 +1,17 @@
+package com.mbarros64.wheater_app_android.data.model
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.RealmClass
+
+@RealmClass
+open class Forecast : RealmObject() {
+    @PrimaryKey
+    var key = ""
+    var city: String = ""
+    var head: String = ""
+    var description: String = ""
+    var date: Long = 0L
+    var temperature: Double = 0.0
+    var imageUrl: String = ""
+}
